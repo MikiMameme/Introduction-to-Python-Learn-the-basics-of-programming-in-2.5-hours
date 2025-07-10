45bf1e3e09bb85790e3df33aacaff09c29aa5792
神酒まめ（Miki Mame） <milky.is.mama.flavor@gmail.com>
2025-07-10 19:45:15 +0900

    script.py

---
+++ script.py
@@ -0,0 +1,28 @@
+print("あいうえお") #ここはコメント文です
+print("かきくけこ")
+print("さしすせそ")
+#purint関数の後ろ
+
+
+apple_price = 100
+text = f"このリンゴは{apple_price}円です"
+print(text)
+
+apple_price = 100
+a_type = type(apple_price)
+
+
+name = "斎藤"
+n_type = type(name)
+
+weight = 54.5
+w_type = type(weight)
+
+print(a_type, n_type, w_type)
+
+
+math = 82
+japanese = 74
+english = 60
+avg_score = ( math + japanese + english ) / 3
+print(avg_score)
